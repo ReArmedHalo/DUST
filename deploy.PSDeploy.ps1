c9a071ff7d40c0fd98f198352ed3ee6284f645c6
Dustin Schreiber <1661728+ReArmedHalo@users.noreply.github.com>
2019-03-06 23:03:41 -0500

    Debug be gone
    Removing debug stuff

--- deploy.PSDeploy.ps1
+++ deploy.PSDeploy.ps1
@@ -1,12 +1,9 @@
-Write-Host "Get-ChildItems"
-Get-ChildItem env:
-
 Deploy Module {
     By PSGalleryModule {
         FromSource $ENV:BHModulePath
         To PSGallery
         WithOptions @{
-            ApiKey = $ENV:NuGetApiKey
+            ApiKey = $env:NuGetApiKey
         }
     }
 }
