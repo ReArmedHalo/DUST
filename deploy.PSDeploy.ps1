0b7e31919611424e88dfdd470be97dfd4c16833e
Dustin <1661728+ReArmedHalo@users.noreply.github.com>
2019-03-19 10:34:38 -0400

    Further debugging
    !deploy

--- deploy.PSDeploy.ps1
+++ deploy.PSDeploy.ps1
@@ -3,7 +3,7 @@ Deploy Module {
         FromSource $ENV:BHModulePath
         To PSGallery
         WithOptions @{
-            ApiKey = $ENV:NuGetApiKey
+            NuGetApiKey = $ENV:NuGetApiKey
         }
     }
 }
