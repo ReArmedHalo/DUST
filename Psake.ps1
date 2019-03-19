72efe6ee5072a083ee1a90e4abdbf4146bdf0a43
Dustin <1661728+ReArmedHalo@users.noreply.github.com>
2019-03-19 15:23:07 -0400

    Build Docs before tests

--- Psake.ps1
+++ Psake.ps1
@@ -39,7 +39,7 @@ Task Init {
     "`n"
 }
 
-Task Test -Depends Init  {
+Task Test -Depends Init, BuildDocs  {
     $lines
     "`n`tSTATUS: Testing with PowerShell $PSVersion"
 
@@ -117,4 +117,16 @@ Task Deploy -Depends Build {
         "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
         "`t* Your commit message includes !deploy (Current: $ENV:BHCommitMessage)"
     }
+}
+
+Task BuildDocs -Depends Init {
+    $lines
+
+    $Params = @{
+        Path = $ProjectRoot
+        Recurse = $false
+        Force = $true
+        Tags = docs
+    }
+    Invoke-PSDeploy @Verbose @Params
 }
