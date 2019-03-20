8ac16ee5b7b4affd83e9e369729921e0d2ac66d0
Dustin <1661728+ReArmedHalo@users.noreply.github.com>
2019-03-20 13:54:12 -0400

    Azure AD and Documentation
    Azure changes, aligning more with how Microsoft refers to these services. Function renaming (Connect-AzureADDelegatedTenant)

--- DUST/public/Exchange Online/Connect-ExchangeOnline.ps1
+++ DUST/public/Exchange Online/Connect-ExchangeOnline.ps1
@@ -13,18 +13,22 @@ Function Connect-ExchangeOnline {
         [Parameter(ParameterSetName='Delegated',Mandatory,Position=2)]
         [PSCredential] $Credential
     )
-    
+
     Remove-BrokenOrClosedDUSTPSSessions
-    
-    if ($Delegated) {
-        # MFA is not supported using delegated admin
-        # Saving this for my hopes and dreams of it one day being supported
-        #$EXOSession = New-ExoPSSession -ConnectionUri 'https://ps.outlook.com/powershell-liveid?DelegatedOrg=$ClientDomain'
-        $EXOSession = New-PSSession -Name 'DUST-EXO' -ConfigurationName Microsoft.Exchange -ConnectionUri ('https://ps.outlook.com/powershell-liveid?DelegatedOrg='+$ClientDomain) -Credential $Credential -Authentication Basic -AllowRedirection        
-    } else {
-        # We only need the Exchange Online PS module if we are using MFA
-        Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+'\Apps\2.0\') -Filter 'Microsoft.Exchange.Management.ExoPowershellModule.dll' -Recurse).FullName | Where-Object {$_ -notmatch '_none_'} | Select-Object -First 1)
-        $EXOSession = New-ExoPSSession
+
+    try {
+        if ($Delegated) {
+            # MFA is not supported using delegated admin
+            # Saving this for my hopes and dreams of it one day being supported
+            #$EXOSession = New-ExoPSSession -ConnectionUri 'https://ps.outlook.com/powershell-liveid?DelegatedOrg=$ClientDomain'
+            $EXOSession = New-PSSession -Name 'DUST-EXO' -ConfigurationName Microsoft.Exchange -ConnectionUri ('https://ps.outlook.com/powershell-liveid?DelegatedOrg='+$ClientDomain) -Credential $Credential -Authentication Basic -AllowRedirection
+        } else {
+            # We only need the Exchange Online PS module if we are using MFA
+            Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+'\Apps\2.0\') -Filter 'Microsoft.Exchange.Management.ExoPowershellModule.dll' -Recurse).FullName | Where-Object {$_ -notmatch '_none_'} | Select-Object -First 1)
+            $EXOSession = New-ExoPSSession
+        }
+        Import-Module (Import-PSSession $EXOSession -AllowClobber -DisableNameChecking) -DisableNameChecking -Global
+    } catch {
+        Write-Error $_
     }
-    Import-Module (Import-PSSession $EXOSession -AllowClobber -DisableNameChecking) -DisableNameChecking -Global
 }
