8954392cddf91fbdc245aa5bdc314c2898d04559
Dustin <1661728+ReArmedHalo@users.noreply.github.com>
2019-04-16 16:38:15 -0400

    Azure AD Group Administration Activities

--- DUST/private/Microsoft 365 Health Check/Get-MS365HCAzureADGroupAdministrationActivities.ps1
+++ DUST/private/Microsoft 365 Health Check/Get-MS365HCAzureADGroupAdministrationActivities.ps1
@@ -11,4 +11,24 @@ Function Get-MS365HCAzureADGroupAdministrationActivities {
         [String] $StartDate
     )
 
+    try {
+        $graphRequestUri = "https://graph.microsoft.com/beta/auditlogs/directoryaudits?`$filter=activityDateTime gt $StartDate and loggedByService eq 'Core Directory' and category eq 'GroupManagement' and (activityDisplayName eq 'Add member to group' or activityDisplayName eq 'Remove member from group' or activityDisplayName eq 'Add owner to group' or activityDisplayName eq 'Remove owner from group')"
+        $response = Invoke-WebRequest -Method 'GET' -Uri $graphRequestUri -ContentType "application/json" -Headers @{Authorization = "Bearer $AccessToken"} -ErrorAction Stop
+        $json = ($response.Content | ConvertFrom-Json)
+        $results = $json.Value
+        $outputData = @()
+        foreach ($record in $results) {
+            $entry = New-Object PSObject -Property @{
+                activityDateTime = $record.activityDateTime
+                activityDisplayName = $record.activityDisplayName
+                operationType = $record.operationType
+                initiatedBy = $record.initiatedBy.user.userPrincipalName
+                targetResources = ($record.targetResources | Where-Object {$_.type -eq 'user'}).userPrincipalName
+            }
+            $outputData += $entry
+        }
+        $outputData | Export-Csv -Path "$OutputPath\UserAdministrationActivities.csv" -NoTypeInformation
+    } catch {
+        Write-Error $_
+    }
 }
