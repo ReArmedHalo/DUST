e688ebc17ecc8c4aa33979c22254c7e629c708b0
Dustin <1661728+ReArmedHalo@users.noreply.github.com>
2019-07-22 13:55:58 -0400

    Group Support
    Show the group that was modified, show group creations and deletions, handle application modification of group support

--- DUST/private/Microsoft 365 Health Check/Get-MS365HCAzureADGroupAdministrationActivities.ps1
+++ DUST/private/Microsoft 365 Health Check/Get-MS365HCAzureADGroupAdministrationActivities.ps1
@@ -12,19 +12,51 @@ Function Get-MS365HCAzureADGroupAdministrationActivities {
     )
 
     try {
-        $graphRequestUri = "https://graph.microsoft.com/beta/auditlogs/directoryaudits?`$filter=activityDateTime gt $StartDate and loggedByService eq 'Core Directory' and category eq 'GroupManagement' and (activityDisplayName eq 'Add member to group' or activityDisplayName eq 'Remove member from group' or activityDisplayName eq 'Add owner to group' or activityDisplayName eq 'Remove owner from group')"
+        $graphRequestUri = "https://graph.microsoft.com/beta/auditlogs/directoryaudits?`$filter=activityDateTime gt $StartDate and loggedByService eq 'Core Directory' and category eq 'GroupManagement' and (activityDisplayName eq 'Add member to group' or activityDisplayName eq 'Remove member from group' or activityDisplayName eq 'Add owner to group' or activityDisplayName eq 'Remove owner from group' or activityDisplayName eq 'Add group' or activityDisplayName eq 'Delete group')"
         $response = Invoke-WebRequest -Method 'GET' -Uri $graphRequestUri -ContentType "application/json" -Headers @{Authorization = "Bearer $AccessToken"} -ErrorAction Stop
         $json = ($response.Content | ConvertFrom-Json)
         $results = $json.Value
+
         $outputData = @()
         foreach ($record in $results) {
             $entry = New-Object PSObject -Property @{
                 activityDateTime = $record.activityDateTime
                 activityDisplayName = $record.activityDisplayName
                 operationType = $record.operationType
-                initiatedBy = $record.initiatedBy.user.userPrincipalName
-                targetResources = ($record.targetResources | Where-Object {$_.type -eq 'user'}).userPrincipalName
+                targetGroup = $null
+                targetResource = $null
+                targetResourceType = $null
+                initiatedBy = $null
+                initiatedByType = $null
             }
+            
+            if (($record.activityDisplayName -like '*from group') -or ($record.activityDisplayName -like '*to group')) {
+                # Add/Remove members
+                $entry.targetGroup = (Get-AzureADGroup -ObjectId ($record.targetResources | Where-Object {$_.type -eq 'Group'}).id).DisplayName
+            } else {
+                # Add/Delete group
+                $entry.targetGroup = ($record.targetResources | Where-Object {$_.type -eq 'Group'}).DisplayName
+            }
+
+            if ($record.targetResources.type -contains 'Device') { # Device
+                $entry.targetResource = ($record.targetResources | Where-Object {$_.type -eq 'Device'}).displayName
+                $entry.targetResourceType = 'Device'
+            } elseif ($record.targetResources.type -contains 'User') { # User
+                $entry.targetResource = ($record.targetResources | Where-Object {$_.type -eq 'User'}).userPrincipalName
+                $entry.targetResourceType = 'User'
+            } elseif ($record.targetResources.type -contains 'Group') { # Group
+                $entry.targetResource = ($record.targetResources | Where-Object {$_.type -eq 'Group'}).displayName
+                $entry.targetResourceType = 'Group'
+            }
+
+            if ($record.initiatedBy.app) {
+                $entry.initiatedBy = $record.initiatedBy.app.displayName
+                $entry.initiatedByType = 'App'
+            } elseif ($record.initiatedBy.user) {
+                $entry.initiatedBy = $record.initiatedBy.user.userPrincipalName
+                $entry.initiatedByType = 'User'
+            }
+
             $outputData += $entry
         }
         $outputData | Export-Csv -Path "$OutputPath\AzureADGroupAdministrationActivities.csv" -NoTypeInformation
