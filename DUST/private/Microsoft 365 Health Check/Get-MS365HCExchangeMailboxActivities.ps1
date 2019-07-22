27b4c38704de0a8ea4ba5b8145adff3ea914553d
Dustin <1661728+ReArmedHalo@users.noreply.github.com>
2019-07-22 10:06:50 -0400

    Fixing Fetched Properties
    Making things look nicer mostly, making it clear who did what to which mailbox and with which user (say that five times fast). Hiding DiscoverySearchMailbox audit data as it can clutter the report and typically isn't required. Maybe this will be made optional at some point.

--- DUST/private/Microsoft 365 Health Check/Get-MS365HCExchangeMailboxActivities.ps1
+++ DUST/private/Microsoft 365 Health Check/Get-MS365HCExchangeMailboxActivities.ps1
@@ -17,16 +17,17 @@ Function Get-MS365HCExchangeMailboxActivities {
         foreach ($parameter in $auditData.Parameters) {
             $parameters | Add-Member -NotePropertyName $parameter.Name -NotePropertyValue $parameter.Value
         }
-        $hash = [Ordered]@{
-            CreationTime = $auditData.CreationTime
-            Operation = $auditData.Operation
-            UserId = $auditData.UserId
-            UserPrincipalName = (Get-Mailbox -Identity $auditData.Identity | Select-Object UserPrincipalName)
-            User = $parameters.User
-            AccessRights = $parameters.AccessRights
+        if ($parameters.Identity -notlike "*DiscoverySearchMailbox*") {
+            $hash = [Ordered]@{
+                CreationTime = $auditData.CreationTime
+                Operation = $auditData.Operation
+                InitiatorUserId = $auditData.UserId
+                TargetUserPrincipalName = (Get-Mailbox -Identity $parameters.Identity).UserPrincipalName
+                User = (Get-Mailbox -Identity $parameters.User).UserPrincipalName
+                AccessRights = $parameters.AccessRights
+            }
+            $outputData += New-object PSObject -Property $hash
         }
-        $outputEntry = New-object PSObject -Property $hash
-        $outputData += $outputEntry
     }
     $outputData | Export-Csv -Path "$OutputPath\ExchangeMailboxActivities.csv" -NoTypeInformation
 }
