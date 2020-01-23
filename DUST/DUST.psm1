43a39aa0b42eafb3809b9defc85013886cb5816c
Dustin Schreiber <dustin@schreiber.us>
2020-01-23 11:45:42 -0500

    Testing an update checker

--- DUST/DUST.psm1
+++ DUST/DUST.psm1
@@ -9,5 +9,15 @@ foreach ($import in @($public + $private)) {
     }
 }
 
+$local = Get-Module -Name DUST
+if ($local) { # In case the module is loaded via source
+    $online = Find-Module -Repository PSGallery -Name DUST
+    if ($online.Version -gt $local.Version) {
+        Write-Host "DUST Module Update Available!"
+        Write-Host "Latest: $($online.Version)"
+        Write-Host "Installed: $($local.Version)"
+    } 
+}
+
 New-Alias -Name Connect-DUSTAzureAD -Value Connect-AzureAD
 Export-ModuleMember -Alias Connect-DUSTAzureAD -Function $public.BaseName
