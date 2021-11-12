03fd2e85b1c2a7e2d9f40be0ea2c8afc7111f265
Dustin Schreiber <dustin@schreiber.us>
2021-11-12 14:00:28 -0500

    Update Get-MFAReport.ps1
    Fixes #5 - More work to do on this function still but this fixes that issue at least

--- DUST/public/Exchange Online/Get-MFAReport.ps1
+++ DUST/public/Exchange Online/Get-MFAReport.ps1
@@ -5,26 +5,16 @@
 <# INCOMPLETE #>
 Function Get-MFAReport {
     [CmdletBinding()] Param (
-        [Parameter()]
-        [String] $ExportTo
+        [Parameter(Mandatory)]
+        [String] $Path
     )
 
-    Write-Progress -Activity "Fetching..." -CurrentOperation "Fetching shared and system mailboxes from Exchange Online..." -PercentComplete -1
     $shared = Get-Mailbox -RecipientTypeDetails SharedMailbox,RoomMailbox,EquipmentMailbox,DiscoveryMailbox -ResultSize Unlimited | Select-Object UserPrincipalName
-    Write-Progress -Activity "Fetching..." -CurrentOperation "Fetching users from Azure AD..." -PercentComplete -1
-    $users = Get-MsolUser | Select-Object DisplayName,UserPrincipalName,@{Name="MultifactorAuthentication";Expression={$(if($_.StrongAuthenticationMethods.Count -eq 0){ "Unregistered" } else { "Registered" })}}
+    $users = Get-MsolUser -All:$true | Select-Object DisplayName,UserPrincipalName,@{Name="MultifactorAuthentication";Expression={$(if($_.StrongAuthenticationMethods.Count -eq 0){ "Unregistered" } else { "Registered" })}}
 
-    Write-Progress -Activity "Compiling..." -Status "Sorting through the data..." -PercentComplete 0
-    $i = 1
-    $percentComplete = 0
-    $stepInterval = (100 / $users.Count)
     foreach ($user in $users) {
-        start-sleep 2
-        Write-Progress -Activity "Compiling..." -Status "Sorting through the data..." -CurrentOperation "Processing user $($user.UserPrincipalName) ($i of $($users.Count))" -PercentComplete $percentComplete
-        if ($shared.UserPrincipalName -contains $user.UserPrincpalName) {
-            $user | Export-Csv -Append -Path .\mfa.csv -NoTypeInformation -ErrorAction Stop
+        if ($shared.UserPrincipalName -notcontains $user.UserPrincpalName) {
+            $user | Export-Csv -Append -Path $Path -NoTypeInformation -ErrorAction Stop
         }
-        $percentComplete += $stepInterval
-        $i++
     }
 }
